f02c4574a8a7f375b64d74625f437fd72b5f0e0d
tyyulovelva <black76327@gmail.com>
2017-07-02 20:26:54 -0500

    Add files via upload

---
+++ TPF.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace TenPointHalf
+{
+    partial class TPF
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picboxComputer1 = new System.Windows.Forms.PictureBox();
+            this.picboxComputer2 = new System.Windows.Forms.PictureBox();
+            this.picboxComputer3 = new System.Windows.Forms.PictureBox();
+            this.picboxComputer4 = new System.Windows.Forms.PictureBox();
+            this.picboxComputer5 = new System.Windows.Forms.PictureBox();
+            this.picboxPlayer5 = new System.Windows.Forms.PictureBox();
+            this.picboxPlayer4 = new System.Windows.Forms.PictureBox();
+            this.picboxPlayer3 = new System.Windows.Forms.PictureBox();
+            this.picboxPlayer2 = new System.Windows.Forms.PictureBox();
+            this.picboxPlayer1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnWant = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picboxComputer1
+            // 
+            this.picboxComputer1.Location = new System.Drawing.Point(201, 12);
+            this.picboxComputer1.Name = "picboxComputer1";
+            this.picboxComputer1.Size = new System.Drawing.Size(270, 325);
+            this.picboxComputer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxComputer1.TabIndex = 0;
+            this.picboxComputer1.TabStop = false;
+            // 
+            // picboxComputer2
+            // 
+            this.picboxComputer2.Location = new System.Drawing.Point(477, 12);
+            this.picboxComputer2.Name = "picboxComputer2";
+            this.picboxComputer2.Size = new System.Drawing.Size(270, 325);
+            this.picboxComputer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxComputer2.TabIndex = 1;
+            this.picboxComputer2.TabStop = false;
+            // 
+            // picboxComputer3
+            // 
+            this.picboxComputer3.Location = new System.Drawing.Point(753, 12);
+            this.picboxComputer3.Name = "picboxComputer3";
+            this.picboxComputer3.Size = new System.Drawing.Size(270, 325);
+            this.picboxComputer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxComputer3.TabIndex = 2;
+            this.picboxComputer3.TabStop = false;
+            // 
+            // picboxComputer4
+            // 
+            this.picboxComputer4.Location = new System.Drawing.Point(1029, 12);
+            this.picboxComputer4.Name = "picboxComputer4";
+            this.picboxComputer4.Size = new System.Drawing.Size(270, 325);
+            this.picboxComputer4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxComputer4.TabIndex = 3;
+            this.picboxComputer4.TabStop = false;
+            // 
+            // picboxComputer5
+            // 
+            this.picboxComputer5.Location = new System.Drawing.Point(1305, 12);
+            this.picboxComputer5.Name = "picboxComputer5";
+            this.picboxComputer5.Size = new System.Drawing.Size(270, 325);
+            this.picboxComputer5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxComputer5.TabIndex = 4;
+            this.picboxComputer5.TabStop = false;
+            // 
+            // picboxPlayer5
+            // 
+            this.picboxPlayer5.Location = new System.Drawing.Point(1305, 383);
+            this.picboxPlayer5.Name = "picboxPlayer5";
+            this.picboxPlayer5.Size = new System.Drawing.Size(270, 325);
+            this.picboxPlayer5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxPlayer5.TabIndex = 9;
+            this.picboxPlayer5.TabStop = false;
+            // 
+            // picboxPlayer4
+            // 
+            this.picboxPlayer4.Location = new System.Drawing.Point(1029, 383);
+            this.picboxPlayer4.Name = "picboxPlayer4";
+            this.picboxPlayer4.Size = new System.Drawing.Size(270, 325);
+            this.picboxPlayer4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxPlayer4.TabIndex = 8;
+            this.picboxPlayer4.TabStop = false;
+            // 
+            // picboxPlayer3
+            // 
+            this.picboxPlayer3.Location = new System.Drawing.Point(753, 383);
+            this.picboxPlayer3.Name = "picboxPlayer3";
+            this.picboxPlayer3.Size = new System.Drawing.Size(270, 325);
+            this.picboxPlayer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxPlayer3.TabIndex = 7;
+            this.picboxPlayer3.TabStop = false;
+            // 
+            // picboxPlayer2
+            // 
+            this.picboxPlayer2.Location = new System.Drawing.Point(477, 383);
+            this.picboxPlayer2.Name = "picboxPlayer2";
+            this.picboxPlayer2.Size = new System.Drawing.Size(270, 325);
+            this.picboxPlayer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxPlayer2.TabIndex = 6;
+            this.picboxPlayer2.TabStop = false;
+            // 
+            // picboxPlayer1
+            // 
+            this.picboxPlayer1.Location = new System.Drawing.Point(201, 383);
+            this.picboxPlayer1.Name = "picboxPlayer1";
+            this.picboxPlayer1.Size = new System.Drawing.Size(270, 325);
+            this.picboxPlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxPlayer1.TabIndex = 5;
+            this.picboxPlayer1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 324);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "電腦";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 254);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "玩家";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnWant
+            // 
+            this.btnWant.Location = new System.Drawing.Point(11, 649);
+            this.btnWant.Name = "btnWant";
+            this.btnWant.Size = new System.Drawing.Size(92, 58);
+            this.btnWant.TabIndex = 12;
+            this.btnWant.Text = "要牌";
+            this.btnWant.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(104, 649);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(92, 58);
+            this.btnEnd.TabIndex = 13;
+            this.btnEnd.Text = "OK";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // TPF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1587, 720);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnWant);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picboxPlayer5);
+            this.Controls.Add(this.picboxPlayer4);
+            this.Controls.Add(this.picboxPlayer3);
+            this.Controls.Add(this.picboxPlayer2);
+            this.Controls.Add(this.picboxPlayer1);
+            this.Controls.Add(this.picboxComputer5);
+            this.Controls.Add(this.picboxComputer4);
+            this.Controls.Add(this.picboxComputer3);
+            this.Controls.Add(this.picboxComputer2);
+            this.Controls.Add(this.picboxComputer1);
+            this.Name = "TPF";
+            this.Tag = "a";
+            this.Text = "TenPointHalf";
+            this.Load += new System.EventHandler(this.frmTPF_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxComputer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picboxComputer1;
+        private System.Windows.Forms.PictureBox picboxComputer2;
+        private System.Windows.Forms.PictureBox picboxComputer3;
+        private System.Windows.Forms.PictureBox picboxComputer4;
+        private System.Windows.Forms.PictureBox picboxComputer5;
+        private System.Windows.Forms.PictureBox picboxPlayer5;
+        private System.Windows.Forms.PictureBox picboxPlayer4;
+        private System.Windows.Forms.PictureBox picboxPlayer3;
+        private System.Windows.Forms.PictureBox picboxPlayer2;
+        private System.Windows.Forms.PictureBox picboxPlayer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnWant;
+        private System.Windows.Forms.Button btnEnd;
+    }
+}
+
---
+++ TPF.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Poker;
+
+namespace TenPointHalf
+{
+    public partial class TPF : Form
+    {
+        private Poker cards = new Poker();
+        public TPF()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTPF_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
